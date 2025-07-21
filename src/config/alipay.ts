e4f614aa37562321ccc5a2eb89e543de380615bc
HUHU <wzqwzq10122216@qq.com>
2025-07-21 22:07:25 +0800

    1

--- src/config/alipay.ts
+++ src/config/alipay.ts
@@ -31,10 +31,10 @@ export const alipaySandboxConfig = {
   privateKey: process.env.ALIPAY_SANDBOX_PRIVATE_KEY || '',
   publicKey: process.env.ALIPAY_SANDBOX_PUBLIC_KEY || '',
   gateway: 'https://openapi-sandbox.dl.alipaydev.com/gateway.do',
-  returnUrl: 'https://study-platform-five.vercel.app/payment/result', // 支付结果页面
-  notifyUrl: 'https://study-platform-five.vercel.app/api/payment/alipay/notify', // 异步通知接口
-  registerNotifyUrl: 'https://study-platform-five.vercel.app/api/payment/register-notify', // 注册订单异步通知接口
-  registerReturnUrl: 'https://study-platform-five.vercel.app/payment/register-result', // 注册订单支付结果页面
+  returnUrl: 'http://154.201.73.21:3000/payment/result', // 支付结果页面
+  notifyUrl: 'http://154.201.73.21:3000/api/payment/alipay/notify', // 异步通知接口
+  registerNotifyUrl: 'http://154.201.73.21:3000/api/payment/register-notify', // 注册订单异步通知接口
+  registerReturnUrl: 'http://154.201.73.21:3000/payment/register-result', // 注册订单支付结果页面
 };
 
 // 支付宝正式环境配置
@@ -43,8 +43,8 @@ export const alipayProdConfig = {
   privateKey: process.env.ALIPAY_SANDBOX_PRIVATE_KEY || '',
   publicKey: process.env.ALIPAY_SANDBOX_PUBLIC_KEY || '',
   gateway: 'https://openapi.alipay.com/gateway.do',
-  returnUrl: 'https://study-platform-five.vercel.app/payment/result', // 支付结果页面
-  notifyUrl: 'https://study-platform-five.vercel.app/api/payment/alipay/notify', // 异步通知接口
-  registerNotifyUrl: 'https://study-platform-five.vercel.app/api/payment/register-notify',// 注册订单异步通知接口
-  registerReturnUrl: 'https://study-platform-five.vercel.app/payment/result', // 注册订单支付结果页面
+  returnUrl: 'http://154.201.73.21:3000/payment/result', // 支付结果页面
+  notifyUrl: 'http://154.201.73.21:3000/api/payment/alipay/notify', // 异步通知接口
+  registerNotifyUrl: 'http://154.201.73.21:3000/api/payment/register-notify',// 注册订单异步通知接口
+  registerReturnUrl: 'http://154.201.73.21:3000/payment/result', // 注册订单支付结果页面
 }; 
